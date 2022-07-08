ddeea54db54b7f8ee99915d788ec3ed6d9d2491d
muammadibal <muammadibal@gmail.com>
2022-07-08 07:15:26 +0700

    fix responsive notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -9,8 +9,8 @@ const Notification = ({onClick}) => {
                 I write articles related to web technologies, such as design trends, development tools, UI/UX case studies and reviews, and more. Sign up to my newsletter to get them all.
             </p>
             <div>
-                <input placeholder='Email Address' style={{padding: '5px 10px', border: 'none', marginRight: 5, width: '70%'}}/>
-                <button style={{ border: 'none', color: 'white', background: '#FBA200', padding: '5px 10px', cursor: 'pointer' }}>Count me in!</button>
+                <input placeholder='Email Address' className='notification-input'/>
+                <button className='notification-btn'>Count me in!</button>
             </div>
         </div>
     )
